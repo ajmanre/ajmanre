88d3fa5c89d44762d60086f6b5de9ee1cbc0b162
Mohammed Mujahed Khan <mujahed08@hotmail.com>
2022-07-04 12:34:47 +0530

    change password service

--- src/main/java/com/ajmanre/controllers/UserController.java
+++ src/main/java/com/ajmanre/controllers/UserController.java
@@ -4,13 +4,15 @@ import com.ajmanre.models.Requirement;
 import com.ajmanre.models.RoleEnum;
 import com.ajmanre.payload.response.Page;
 import com.ajmanre.repository.UserRepository;
+import com.ajmanre.security.jwt.JwtUtils;
 import com.ajmanre.services.UserService;
 import org.openapitools.api.UserApi;
-import org.openapitools.model.RequirementPage;
-import org.openapitools.model.User;
-import org.openapitools.model.UserPage;
+import org.openapitools.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,6 +32,12 @@ public class UserController implements UserApi {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
 
     @GetMapping("/justLegacyAPI")
     public ResponseEntity<Page<com.ajmanre.payload.response.User>> get(@RequestParam(defaultValue = "0") int pageNo, @RequestParam(defaultValue = "10") int size) {
@@ -73,5 +81,28 @@ public class UserController implements UserApi {
 
         return new User().id(user.getId()).updatedAt(updatedAt).name(user.getName()).username(user.getUsername())
                 .email(user.getEmail()).roles(roles);
+    }
+
+    @Override
+    public ResponseEntity<MessageResponse> changePassword(ChangePassword changePassword, String authorization) {
+
+        if (StringUtils.hasText(authorization) && authorization.startsWith("Bearer ")) {
+            String jwt = authorization.substring(7, authorization.length());
+            final String username = jwtUtils.getUserNameFromJwtToken(jwt);
+
+            boolean isAdmin = userRepository.findByUsername(username).get()
+                    .getRoles().stream().anyMatch(r -> r.getName().equals(RoleEnum.ROLE_ADMIN));
+
+
+            if(username.equals(changePassword.getUsername()) || isAdmin) {
+                com.ajmanre.models.User user = userRepository.findByUsername(changePassword.getUsername()).get();
+                user.setPassword(encoder.encode(changePassword.getPassword()));
+                userRepository.save(user);
+                return ResponseEntity.ok(new MessageResponse().message("Password changed successfully").identifier(user.getId()));
+            }
+
+        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(new MessageResponse().message("Change password can only be performed by admin or user itself."));
     }
 }
